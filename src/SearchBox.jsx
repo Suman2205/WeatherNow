32646bb8ea9906e287373eccd99885ab5b264da3
Suman2205 <suman27574@gmail.com>
2025-11-06 21:09:52 +0530

    Updation Commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 export default function SearchBox({updateInfo}){
     let [city,setCity]=useState("");
-    const API_URL="http://api.openweathermap.org/data/2.5/weather";
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY="fb11bc9c3a726fc35b26232f3e7f4cc0";
     let getWeatherInfo=async()=>{
         let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
